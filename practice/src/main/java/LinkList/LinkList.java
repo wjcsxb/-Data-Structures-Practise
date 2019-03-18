1b849034ee074430b0a4666fb0028cb673c68fb5
junchaowang <1406175809@qq.com>
2019-03-18 10:33:46 +0800

    单链表；

--- practice/src/main/java/LinkList/LinkList.java
+++ practice/src/main/java/LinkList/LinkList.java
@@ -38,6 +38,25 @@ public class LinkList {
         return deleteNode;
     }
     /*
+     * 删除任意位置的节点
+     * */
+    public Node deleteByIndex(int pos){
+        Node currentNode = first;
+        Node previousNode = first;
+        while (index != pos) {
+            previousNode = currentNode;
+            currentNode = previousNode.next;
+            index++;
+        }
+        if (currentNode == first) {
+            first = first.next;
+        } else {
+            index = 0;
+            previousNode.next = currentNode.next;
+        }
+        return currentNode;
+    }
+    /*
      * 插入一个头结点(带头指针)
      * */
     public void addFirstNodeWithHeadNode(int data){
@@ -62,7 +81,32 @@ public class LinkList {
         newNode.next = currentNode;
         index = 0;
     }
+    /*
+    * 根据位置查找节点信息
+    * */
+    public Node findByIndex(int pos) {
+        Node currentNode = first;
+        while (pos != index) {
+            currentNode = currentNode.next;
+            index++;
+        }
+        index = 0;
+        return currentNode;
+    }
 
+    /*
+     * 根据数据查找节点信息
+     * */
+    public Node findByData(int data) {
+        Node currentNode = first;
+        while (currentNode.data != data) {
+            if (currentNode.next == null) {
+                return null;
+            }
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
     /*
     * 显示所有节点信息
     * */
@@ -75,11 +119,14 @@ public class LinkList {
 
     }
     public static void main(String[] args){
-//        LinkList linkList = new LinkList();
-        LinkList linkList = new LinkList(0);
-//        linkList.addFirstNode(20);
-        linkList.addFirstNodeWithHeadNode(20);
-        linkList.addFirstNodeWithHeadNode(21);
+        LinkList linkList = new LinkList();
+//        LinkList linkList = new LinkList(0);
+        linkList.addFirstNode(20);
+        linkList.addFirstNode(21);
+        linkList.addFirstNode(19);
+        linkList.add(1,22);
+//        linkList.addFirstNodeWithHeadNode(20);
+//        linkList.addFirstNodeWithHeadNode(21);
 //        linkList.addFirstNode(21);
         linkList.displayAllNodes();
     }
